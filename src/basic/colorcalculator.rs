cef0b73985f7ce65106c6352d218bc803a8467d7
Nyári Dávid Tamás <nyaridavid@gmail.com>
2018-01-01 20:59:53 +0100

    Previous commit message wrong. Simple Illuminator was implemented Color Calculator still missing, commented it out for build fix

--- src/basic/colorcalculator.rs
+++ src/basic/colorcalculator.rs
@@ -2,22 +2,22 @@ use core::*;
 use tools::*;
 
 
-pub struct SimpleColorCalculator {
+// pub struct SimpleColorCalculator {
 
-}
+// }
 
-impl SimpleColorCalculator {
-    fn get_ambient_color(model: &Model, ray_caster: &RayCaster) -> Option<Color> {
+// impl SimpleColorCalculator {
+//     fn get_ambient_color(model: &Model, ray_caster: &RayCaster) -> Option<Color> {
         
-    }
-}
+//     }
+// }
 
-impl ColorCalculator for SimpleColorCalculator {
-    fn new() -> Self {
-        Self {}
-    }
+// impl ColorCalculator for SimpleColorCalculator {
+//     fn new() -> Self {
+//         Self {}
+//     }
 
-    fn get_color(&self, itersection: &RayIntersection, ray_caster: &RayCaster) -> Option<Color> {
+//     fn get_color(&self, itersection: &RayIntersection, ray_caster: &RayCaster) -> Option<Color> {
 
-    }
-}
+//     }
+// }
