bf68de69974a9e2f75874713a47f6337b1caf727
Nyári Dávid Tamás <nyaridavid@gmail.com>
2018-01-11 21:22:08 +0100

    Started basic light source and Model implementations

--- src/basic/mod.rs
+++ src/basic/mod.rs
@@ -3,6 +3,8 @@ use core::*;
 pub mod intersector;
 pub mod illuminator;
 pub mod colorcalculator;
+pub mod lightsource;
+pub mod model;
 
 pub use self::intersector::*;
 pub use self::illuminator::*;
