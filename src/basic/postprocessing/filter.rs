11b928074c6d66b29fa324b7dfa7a3ab7197cbe2
Nyári Dávid Tamás <nyaridavid@gmail.com>
2018-04-23 00:15:02 +0200

    Started Media filter implementation

---
+++ src/basic/postprocessing/filter.rs
@@ -0,0 +1,12 @@
+// use defs::{IntType}
+// use core::{SceneBuffer, BasicSceneBuffer};
+
+// struct MedianFilter<'obuffer> {
+//     original_buffer: &'obuffer SceneBuffer,
+//     rect_radius: IntType,
+//     result_buffer: BasicSceneBuffer
+// }
+
+// impl<'obuffer> MedianFilter<'obuffer> {
+//     pub fn new(original_buffer: &SceneBuffer, rect_radius: IntType)
+// }
